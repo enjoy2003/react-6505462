584d2ba9ad62e72c7875c7e93692044603caa5ba
enjoy2003 <joypijitra2546@gmail.com>
2024-09-26 10:13:27 +0700

    เปลี่ยนหัวข้อเป็นชื่อ-สกุล รหัส

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <div>
       <MyBar name="Me App" />
-      <h1>Hello DIT, RSU</h1>
+      <h1>Pijitra Namsin, 6505462</h1>
       <Grid container spacing={2}>
       {data.map(item => (
         <Grid item xs={12} md={4}>
